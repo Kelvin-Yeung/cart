7041d1c923baef453413b9f456ec8d90536c9ef0
Kelvin-Yeung <y.kelvin2004@gmail.com>
2023-09-19 20:04:17 -0400

    Fixed shop menu bugs

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,17 @@
-import Navbar from "/src/Components/Navbar.jsx";
+// import Navbar from "/src/Components/Navbar.jsx";
 
 export default function About() {
   return (
     <>
-      <Navbar />
-      <h1>About</h1>
+      {/* Navbar */}
+      
+      <img src={"gamepad.svg"}
+      style={{ width: '4rem', height: '80px' }}
+      className="object-cover border border-red-500"
+      />
+      
     </>
   )
 }
+
+//  object-cover object-center
